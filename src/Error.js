924dd5270d39c1348020f99a6a9d27d9345d91fe
Priyanshu Pandey <88549100+rashup198@users.noreply.github.com>
2023-06-26 03:22:34 +0530

    updated Error component

---
+++ src/Error.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <h1 className='text-6xl font-bold text-red-700'>Error 404</h1>
+    </div>
+  )
+}
+
+export default Error
