59800a5b6e827aac0b03422e715761df4712cf8e
Priyanshu Pandey <88549100+rashup198@users.noreply.github.com>
2023-06-26 03:23:53 +0530

    updated heading.js

---
+++ src/components/Heading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Heading = () => {
+  return (
+    <div>
+      <h1 className='text-5xl font-bold text-blue-700 underline text-center '>Image Gallery</h1>
+    </div>
+  )
+}
+
+export default Heading
