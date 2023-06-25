aa07c833966e6228db2ba858b907eebc5ffe725e
Priyanshu Pandey <88549100+rashup198@users.noreply.github.com>
2023-06-26 03:24:33 +0530

    updated Loader component

---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./loader.css"
+
+const Loader = () => {
+  return (
+    <div className='load'>
+      <div className="custom-loader"></div>
+    </div>
+  )
+}
+
+export default Loader
